6f7f7b46f21870f7b2fdc0a6500928db10181c49
daT7F4 <153759092+daT7F4@users.noreply.github.com>
2025-01-18 21:05:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
-#include <vector>
 #include <cmath>
 #include <filesystem>
 
@@ -11,6 +10,7 @@
 
 #include "./src/drawFunctions.hpp"
 #include "./src/synthesis.hpp"
+#include "./src/readFunctions.hpp"
 
 #define u8 uint8_t
 #define u32 uint32_t
@@ -21,15 +21,12 @@ namespace fs = std::filesystem;
 
 float fps = 60.0;
 
-vector<u8> MD;
-vector<vector<uint64_t>> notes;          // timestamp, (channel | note | velocity | on/off)
-vector<vector<uint64_t>> systemMessages; // timestamp, (value index, channel, value, 0)
-vector<vector<uint64_t>> meta;           // timestamp, tempo
-
 u8 header[8] = {0x4D, 0x54, 0x68, 0x64, 0x00, 0x00, 0x00, 0x06};
 u8 trackHeader[4] = {0x4D, 0x54, 0x72, 0x6B};
 
 bool activeNotes[16][128];
+int playingNotes;
+int globalLength;
 
 Color midiColors[16] = {
     Color(255, 87, 34),  // Red-Orange
@@ -50,129 +47,75 @@ Color midiColors[16] = {
     Color(233, 30, 99)   // Pink
 };
 
-uint64_t rI;
 uint64_t noteIndex, metaIndex, systemIndex;
+bool seekT = false;
+bool playT = false;
+bool playing = true;
 
 uint16_t trackCount;
 uint16_t PPQN;
-u32 delta;
-uint64_t trackTime;
-bool report;
 uint64_t MIDITime;
+uint64_t lastMIDITime;
 float Tempo;
 
-u8 eventType;
-u8 channel;
+RtMidiOut midiOut;
 
-uint16_t eight2sixteen(u8 b1, u8 b2) { return b1 << 8 | b2; }
-u32 eight2twentyfour(u8 b1, u8 b2, u8 b3) { return b1 << 16 | b2 << 8 | b3; }
-u32 eight2thirtytwo(u8 b1, u8 b2, u8 b3, u8 b4)
+void updateNotes(bool reverse)
 {
-  return b1 << 24 | b2 << 16 | b3 << 8 | b4;
+  thirdytwo2eight(notes[noteIndex][1]);
+  if (byte4)
+  {
+    playNote(midiOut, byte1, byte2, byte3);
+    if (reverse)
+      playingNotes--;
+    if (!reverse)
+      playingNotes++;
+    activeNotes[byte1][byte2] = !reverse;
+  }
+  else
+  {
+    stopNote(midiOut, byte1, byte2);
+    if (!reverse)
+      playingNotes--;
+    if (reverse)
+      playingNotes++;
+    activeNotes[byte1][byte2] = reverse;
+  }
 }
 
-void getDelta()
+void updateSystem()
 {
-  delta = 0;
-  while (MD[rI] > 127)
+  thirdytwo2eight(systemMessages[systemIndex][1]);
+  switch (byte1)
   {
-    delta = delta << 7 | MD[rI] & 127;
-    rI++;
-  }
-  delta = delta << 7 | MD[rI] & 127;
-  rI++;
-}
-
-void getMeta()
-{
-  rI++;
-  u32 tempo = 0;
-  switch (MD[rI])
-  {
-  case 0x51:
-    tempo = eight2twentyfour(MD[rI + 2], MD[rI + 3], MD[rI + 4]);
-    meta.push_back({trackTime, tempo});
+  case 0:
+    sendProgramChange(midiOut, byte2, byte3);
     break;
-  }
-  rI++;
-  rI += MD[rI];
-  rI++;
-}
-
-void getEvent()
-{
-  if ((MD[rI] & 0xF0) >> 4 > 7)
-  {
-    eventType = (MD[rI] & 0xF0) >> 4;
-    channel = MD[rI] & 0x0F;
-    rI++;
-  }
-  switch (eventType)
-  {
-  case 0b1000: // note off
-    notes.push_back({trackTime, eight2thirtytwo(channel, MD[rI], MD[rI + 1], 0)});
-    rI += 2;
-    break;
-  case 0b1001: // note on
-    if (MD[rI + 1] == 0)
-    {
-      notes.push_back({trackTime, eight2thirtytwo(channel, MD[rI], MD[rI + 1], 0)});
-    }
-    else
-    {
-      notes.push_back({trackTime, eight2thirtytwo(channel, MD[rI], MD[rI + 1], 1)});
-    }
-    rI += 2;
-    break;
-  case 0b1010: // aftertouch
-    rI += 2;
-    break;
-  case 0b1011: // control change
-    rI += 2;
-    break;
-  case 0b1100: // program change
-    systemMessages.push_back({trackTime, eight2thirtytwo(0, channel, MD[rI], 0)});
-    rI += 1;
-    break;
-  case 0b1101: // channel pressure
-    rI += 1;
-    break;
-  case 0b1110: // pitch wheel
-    rI += 2;
-    break;
-  case 0b1111: // system exclusive
-    while (MD[rI] != 0xF7)
-    {
-      rI++;
-    }
-    rI++;
-    break;
-  default:
-    cerr << "Unknown event type 0x" << hex << uppercase << eventType << " at 0x" << hex
-         << rI - 1 << endl;
-    report = true;
   }
 }
 
 int main()
 {
   initFont();
+  loadSprites();
 
   vector<string> files;
 
-  for (const auto &entry : fs::directory_iterator("."))
+  for (const auto &entry : fs::directory_iterator("./midi_files"))
   {
     if (entry.is_regular_file())
     {
       std::string ext = entry.path().extension().string();
       if (ext == ".mid" || ext == ".midi")
       {
-        files.push_back(entry.path().filename().string());
+        files.push_back(entry.path().filename().string().substr(0, 20));
       }
     }
   }
 
-  RenderWindow select(sf::VideoMode(800, 600), "Selection Screen");
+  ContextSettings settings;
+  settings.antialiasingLevel = 0;
+  RenderWindow select(sf::VideoMode(800, 600), "Selection Screen", Style::Default, settings);
   select.setFramerateLimit(60);
 
   int selectedFile = -1;
@@ -186,14 +129,17 @@ int main()
     while (select.pollEvent(event))
     {
       if (event.type == Event::Closed)
+      {
         select.close();
+        return 0;
+      }
     }
     select.clear();
     Vector2i m = Mouse::getPosition(select);
 
     bool startHover = false, exitHover = false;
 
-    if (m.x > 590 && m.x < 790 && m.y > 10 && m.y < 40 && (files.size() > 0 && MIDIDevices.size() > 0))
+    if (m.x > 590 && m.x < 790 && m.y > 10 && m.y < 40 && (files.size() > 0 && MIDIDevices.size() > 0 && selectedDevice != -1 && selectedFile != -1))
     {
       startHover = true;
       if (Mouse::isButtonPressed(sf::Mouse::Left))
@@ -213,13 +159,13 @@ int main()
 
     select.draw(drawRect(5, 90, 790, 1, Color::White));
     select.draw(drawText(5, 5, 80, "TriMIDI", Color::White, 1));
-    select.draw(drawText(330, 5, 20, "v.1.0.1", Color::White, 1));
+    select.draw(drawText(330, 5, 20, "v.1.3", Color::White, 1));
 
-    select.draw(drawRect(590, 10, 200, 30, Color(0, 100 + (startHover * 50) - ((files.size() == 0 || MIDIDevices.size() == 0) * 50), 0)));
+    select.draw(drawRect(590, 10, 200, 30, Color(0, 100 + (startHover * 50) - ((files.size() == 0 || MIDIDevices.size() == 0 || selectedDevice == -1 || selectedFile == -1) * 50), 0)));
     select.draw(drawRect(590, 50, 200, 30, Color(100 + (exitHover * 50), 0, 0)));
 
-    select.draw(drawText(690, 15, 15, "Start", Color::White, 0));
-    select.draw(drawText(690, 55, 15, "Exit", Color::White, 0));
+    select.draw(drawText(690, 16, 16, "Start", Color::White, 0));
+    select.draw(drawText(690, 56, 16, "Exit", Color::White, 0));
 
     if (files.size() > 0)
     {
@@ -237,26 +183,32 @@ int main()
         select.draw(drawRect(40, i * 21 + 100, 350, 20, Color(0, 0, 127 + (hover * 50) + ((i == selectedFile) * 70))));
         select.draw(drawText(40, i * 21 + 100, 18, files[i], Color::White, 1));
       }
-    } else{
+    }
+    else
+    {
       select.draw(drawText(215, 120, 20, "No MIDI Files Found", Color::White, 0));
     }
 
-    if(MIDIDevices.size() > 0){
-    for (int i = 0; i < MIDIDevices.size(); i++)
+    if (MIDIDevices.size() > 0)
     {
-      bool hover = false;
-      if (m.x > 400 && m.x < 750 && m.y > i * 21 + 100 && m.y < i * 21 + 120)
+      for (int i = 0; i < MIDIDevices.size(); i++)
       {
-        hover = true;
-        if (Mouse::isButtonPressed(sf::Mouse::Left))
+        bool hover = false;
+        if (m.x > 400 && m.x < 750 && m.y > i * 21 + 100 && m.y < i * 21 + 120)
         {
-          selectedDevice = i;
+          hover = true;
+          if (Mouse::isButtonPressed(sf::Mouse::Left))
+          {
+            selectedDevice = i;
+          }
         }
+        select.draw(drawRect(400, i * 21 + 100, 350, 20, Color(0, 0, 127 + (hover * 50) + ((i == selectedDevice) * 70))));
+        select.draw(drawText(425, i * 21 + 100, 18, MIDIDevices[i], Color::White, 1));
+        select.draw(drawSprite(midi, 401, i * 21 + 101, 2));
       }
-      select.draw(drawRect(400, i * 21 + 100, 350, 20, Color(0, 0, 127 + (hover * 50) + ((i == selectedDevice) * 70))));
-      select.draw(drawText(400, i * 21 + 100, 18, MIDIDevices[i], Color::White, 1));
     }
-    } else{
+    else
+    {
       select.draw(drawText(575, 120, 20, "No MIDI Devices Found", Color::White, 0));
     }
 
@@ -341,24 +293,20 @@ int main()
   sort(meta.begin(), meta.end(), [](const vector<uint64_t> &a, const vector<uint64_t> &b)
        { return a[0] < b[0]; });
 
+  globalLength = notes[notes.size() - 1][0];
+  float step = 770.0 / (float)globalLength;
+
   cout << "Done decoding" << endl;
   MD.clear();
 
-  RtMidiOut midiOut;
-
   unsigned int nPorts = midiOut.getPortCount();
-  if (nPorts == 0)
-  {
-    std::cerr << "No MIDI output ports available!" << std::endl;
-    return 1;
-  }
 
   midiOut.openPort(selectedDevice);
 
   Tempo = 60000000.0 / meta[0][1];
 
   RenderWindow window(sf::VideoMode(800, 600), "Player");
-  window.setVerticalSyncEnabled(true);
+  window.setFramerateLimit(60);
 
   sf::Clock clock = Clock();
   sf::Time previousTime = clock.getElapsedTime();
@@ -373,49 +321,77 @@ int main()
         window.close();
     }
 
+    Vector2i m = Mouse::getPosition(window);
+
     window.clear();
 
-    window.draw(drawRect(10, 10, 780, 372, Color(15, 15, 15)));
+    window.draw(drawRect(5, 5, 780, 125, Color(15, 15, 15)));
+    window.draw(drawSprite(grid, 10, 10, 1));
 
-    window.draw(drawText(10, 362, 16, to_string(Tempo) + " BPM", Color::White, 1));
-    window.draw(drawText(10, 400, 30, to_string(fps) + "FPS", Color::White, 1));
+    window.draw(drawText(10, 105, 16, to_string(Tempo) + " BPM", Color::White, 1));
+    window.draw(drawText(200, 105, 16, "Playing notes: " + to_string(playingNotes), Color::White, 1));
+    window.draw(drawRect(10, 130, 780, 10, Color(15, 15, 15)));
+    int x = ((float)step * (float)MIDITime) + 10;
+    window.draw(drawSprite(marker, x - 3, 130, 1));
+    if (m.x > x - 5 && m.x < x + 5 && m.y > 130 && m.y < 140 || seekT)
+    {
+      seekT = false;
+      if (Mouse::isButtonPressed(sf::Mouse::Left))
+      {
+        seekT = true;
+        MIDITime = (((float)m.x - 10.0) / (float)step);
+        if (MIDITime != lastMIDITime)
+        {
+          while (notes[noteIndex][0] > MIDITime)
+          {
+            updateNotes(true);
+            noteIndex--;
+          }
+          while (systemMessages[systemIndex][0] > MIDITime)
+          {
+            updateSystem();
+            systemIndex--;
+          }
+          lastMIDITime = MIDITime;
+        }
+      }
 
+    }
+
+    MIDITime = min(MIDITime, (uint64_t)globalLength);
     for (int i = 0; i < 16; i++)
     {
       for (int j = 0; j < 128; j++)
       {
-        if (j % 12 == 0)
-        {
-          window.draw(drawRect(j * 6 + 18, 18, 5, 16 * 21, Color(100, 100, 100)));
-        }
         if (activeNotes[i][j])
         {
-          window.draw(drawRect(j * 6 + 18, i * 21 + 18, 5, 20, midiColors[i]));
+          window.draw(drawRect(11 + (j * 6), 11 + (i * 6), 5, 5, midiColors[i]));
         }
       }
     }
 
+    if(playing)
+      window.draw(drawSprite(play, 10, 150, 2));
+    else
+      window.draw(drawSprite(stop, 10, 150, 2));
+
+    if(m.x > 10 && m.x < 30 && m.y > 150 && m.y < 170){
+      if (Mouse::isButtonPressed(sf::Mouse::Left) && !playT){
+        playing = !playing;
+        playT = true;
+      }
+      if(!Mouse::isButtonPressed(sf::Mouse::Left))
+        playT = false;
+    }
+
     window.display();
 
-    MIDITime += ((Tempo * PPQN) / 60) * (1.0 / fps);
+    if(playing)
+      MIDITime += (((float)Tempo * (float)PPQN) / 60.0) * (1.0 / 60.0);
 
     while (notes[noteIndex][0] < MIDITime && noteIndex < notes.size() - 1)
     {
-      uint32_t value = notes[noteIndex][1];
-      uint8_t byte1 = (value >> 24) & 0xFF;
-      uint8_t byte2 = (value >> 16) & 0xFF;
-      uint8_t byte3 = (value >> 8) & 0xFF;
-      uint8_t byte4 = value & 0xFF;
-      if (byte4)
-      {
-        playNote(midiOut, byte1, byte2, byte3);
-        activeNotes[byte1][byte2] = true;
-      }
-      else
-      {
-        stopNote(midiOut, byte1, byte2);
-        activeNotes[byte1][byte2] = false;
-      }
+      updateNotes(false);
       noteIndex++;
     }
 
@@ -429,23 +405,12 @@ int main()
     }
     while (systemMessages[systemIndex][0] < MIDITime && systemIndex < systemMessages.size() - 1)
     {
-      uint32_t value = systemMessages[systemIndex][1];
-      uint8_t byte1 = (value >> 24) & 0xFF;
-      uint8_t byte2 = (value >> 16) & 0xFF;
-      uint8_t byte3 = (value >> 8) & 0xFF;
-      uint8_t byte4 = value & 0xFF;
-      switch (byte1)
-      {
-      case 0:
-        sendProgramChange(midiOut, byte2, byte3);
-        break;
-      }
+      updateSystem();
       systemIndex++;
     }
-    currentTime = clock.getElapsedTime();
-    fps = 1.0f / (currentTime.asSeconds() - previousTime.asSeconds()); // the asSeconds returns a float
-    previousTime = currentTime;
   }
+
+  stopAllNotes(midiOut);
 
   midiOut.closePort();
 
